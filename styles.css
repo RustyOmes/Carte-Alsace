eca4fcfd24840558032bd43f48f3fddf4c6d2b2c
RustyOmes <168229846+RustyOmes@users.noreply.github.com>
2024-06-18 21:55:05 +0200

    styles.css

---
+++ styles.css
@@ -0,0 +1,19 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+h1 {
+    margin: 20px;
+}
+
+#map {
+    width: 80%;
+    max-width: 1000px;
+    height: 600px;
+    margin-bottom: 20px;
+}
